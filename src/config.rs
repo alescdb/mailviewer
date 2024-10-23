0e707b24eaed7eb77c4b672af2be51d8f36cdca5
Alexandre Del Bigio <alex@cosinus.org>
2024-10-23 17:09:46 +0200

    docker build

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 pub static APP_ID: &str = "org.cosinus.mailviewer";
 pub static VERSION: &str = "0.9.0";
 // pub static GETTEXT_PACKAGE: &str = "mailviewer";
-// pub static LOCALEDIR: &str = "/home/alex/Sources/MailViewer-GITHUB/dist/share/locale";
-pub static PKGDATADIR: &str = "/home/alex/Sources/MailViewer-GITHUB/dist/share/mailviewer";
+// pub static LOCALEDIR: &str = "/usr/local/share/locale";
+pub static PKGDATADIR: &str = "/usr/local/share/mailviewer";
