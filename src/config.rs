7be556a5801cf8953611e5e8150f86962621ac2e
Alexandre Del Bigio <alescdb@users.noreply.github.com>
2024-10-28 13:58:12 +0100

    Makefile changes

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
 pub static APP_ID: &str = "io.github.alescdb.mailviewer";
 pub static VERSION: &str = "0.9.3";
-// pub static GETTEXT_PACKAGE: &str = "mailviewer";
-// pub static LOCALEDIR: &str = "/home/alex/Sources/MailViewer/dist/share/locale";
+#[allow(dead_code)]
+pub static GETTEXT_PACKAGE: &str = "mailviewer";
+#[allow(dead_code)]
+pub static LOCALEDIR: &str = "/home/alex/Sources/MailViewer/dist/share/locale";
 pub static PKGDATADIR: &str = "/home/alex/Sources/MailViewer/dist/share/mailviewer";
